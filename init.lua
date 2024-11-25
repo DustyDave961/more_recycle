84676a5b707d74da434e9137d652ebefb077c3da
DustyDave961 <144002335+DustyDave961@users.noreply.github.com>
2024-11-25 15:09:37 -0600

    Update init.lua
    Minetest is Luanti now, so the "core" table should be referenced instead.

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 local cooking_recipes = {
 }
 
-if minetest.get_modpath("hopper") then
+if core.get_modpath("hopper") then
 	table.insert(cooking_recipes, {"hopper:hopper", "default:steel_ingot 3"})
 	table.insert(cooking_recipes, {"hopper:chute",  "default:steel_ingot 2"})
 	table.insert(cooking_recipes, {"hopper:sorter", "default:steel_ingot 3"})
 end
 
-if minetest.get_modpath("moreores") then
+if core.get_modpath("moreores") then
 	table.insert(cooking_recipes, {"moreores:copper_rail", "nuggets:copper_nugget 3", 3})
-	if minetest.get_modpath("simple_recycle") then
+	if core.get_modpath("simple_recycle") then
 		--mithril tools
 		table.insert(cooking_recipes, {"moreores:axe_mithril",    "moreores:mithril_ingot 3"})
 		table.insert(cooking_recipes, {"moreores:hoe_mithril",    "moreores:mithril_ingot 2"})
@@ -17,13 +17,13 @@ if minetest.get_modpath("moreores") then
 		table.insert(cooking_recipes, {"moreores:shovel_mithril", "moreores:mithril_ingot"})
 		table.insert(cooking_recipes, {"moreores:sword_mithril",  "moreores:mithril_ingot 2"})
 		
-		if minetest.get_modpath("armor_mithril") then
+		if core.get_modpath("armor_mithril") then
 			table.insert(cooking_recipes, {"3d_armor:boots_mithril",      "moreores:mithril_ingot 4"})
 			table.insert(cooking_recipes, {"3d_armor:chestplate_mithril", "moreores:mithril_ingot 8"})
 			table.insert(cooking_recipes, {"3d_armor:helmet_mithril",     "moreores:mithril_ingot 5"})
 			table.insert(cooking_recipes, {"3d_armor:leggings_mithril",   "moreores:mithril_ingot 7"})
 		end
-		if minetest.get_modpath("shields") then
+		if core.get_modpath("shields") then
 			table.insert(cooking_recipes, {"shields:shield_mithril", "moreores:mithril_ingot 7"})
 		end
 
@@ -37,5 +37,5 @@ if minetest.get_modpath("moreores") then
 end
 
 for _, data in pairs(cooking_recipes) do
-	minetest.register_craft({type = "cooking", recipe = data[1], output = data[2], cooktime = data[3] or 4})
+	core.register_craft({type = "cooking", recipe = data[1], output = data[2], cooktime = data[3] or 4})
 end
